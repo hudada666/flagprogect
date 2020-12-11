49678c5574dd290e84866b7323c915a1e74cc4c7
hudada <13940575636@163.com>
2020-12-11 10:54:32 +0800

    Initial commit

---
+++ src/main/java/abc/httpposttool/HttptesttoolApplication.java
@@ -0,0 +1,13 @@
+package abc.httpposttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttptesttoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttptesttoolApplication.class, args);
+	}
+
+}
